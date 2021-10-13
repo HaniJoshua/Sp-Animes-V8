95507d6bcc0a51ee7b35cc2e63f43d5b87f4774f
HaniJoshua <91788215+HaniJoshua@users.noreply.github.com>
2021-10-13 08:54:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,766 @@
+var carta1 = {
+  nome: "Pikachu",
+  imagem:
+    "https://sm.ign.com/ign_br/screenshot/default/pokemon-pikachu-1194135-1280x0_4423.jpg",
+  atributos: {
+    ataque: 20,
+    defesa: 18,
+    poder: 15
+  }
+};
+var carta2 = {
+  nome: "Charizard",
+  imagem:
+    "https://pbs.twimg.com/ext_tw_video_thumb/1376235039575396361/pu/img/3Fwv_eWQmJ3DuIpR.jpg",
+  atributos: {
+    ataque: 29,
+    defesa: 11,
+    poder: 19
+  }
+};
+var carta3 = {
+  nome: "Zeraora",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2018/04/Zeraora-revelado.jpg",
+  atributos: {
+    ataque: 31,
+    defesa: 17,
+    poder: 29
+  }
+};
+var carta4 = {
+  nome: "Tyranitar",
+  imagem:
+    "https://static0.gamerantimages.com/wordpress/wp-content/uploads/2021/08/Pokemon-TCG-Tyranitar-Card-Art-2.jpg",
+  atributos: {
+    ataque: 27,
+    defesa: 19,
+    poder: 17
+  }
+};
+var carta5 = {
+  nome: "Giratina",
+  imagem:
+    "https://1.bp.blogspot.com/-pUtPMQr9lMw/YUCszSL_nOI/AAAAAAAAxl0/WCSnfE9WvWAWht3FTbZSZJFPhq06GZMqgCLcBGAsYHQ/s776/Pokemon-Giratina-and-the-Sky-Warrior.jpg",
+  atributos: {
+    ataque: 27,
+    defesa: 12,
+    poder: 40
+  }
+};
+var carta6 = {
+  nome: "Goku",
+  imagem: "https://sm.ign.com/t/ign_br/screenshot/default/goku_an6e.h720.jpg",
+  atributos: {
+    ataque: 30,
+    defesa: 13,
+    poder: 28
+  }
+};
+var carta7 = {
+  nome: "Majin Vegeta",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/01/20200725-majin-vegeta-1-1200x675-1-910x512.jpg",
+  atributos: {
+    ataque: 35,
+    defesa: 10,
+    poder: 18
+  }
+};
+var carta8 = {
+  nome: "Gogeta",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2018/11/Gogeta-Dragon-Ball-Super-Broly.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 12,
+    poder: 30
+  }
+};
+var carta9 = {
+  nome: "Trunks",
+  imagem: "https://i.ebayimg.com/thumbs/images/g/7ekAAOSwAq9g8Hl6/s-l300.jpg",
+  atributos: {
+    ataque: 25,
+    defesa: 23,
+    poder: 17
+  }
+};
+var carta10 = {
+  nome: "Freeza",
+  imagem: "https://www.einerd.com.br/wp-content/uploads/2018/03/freeza.jpg",
+  atributos: {
+    ataque: 27,
+    defesa: 13,
+    poder: 15
+  }
+};
+var carta11 = {
+  nome: "Naruto Barion",
+  imagem:
+    "https://sm.ign.com/ign_br/screenshot/default/naruto-modo-barion_wbq5.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 20,
+    poder: 39
+  }
+};
+var carta12 = {
+  nome: "Isshiki",
+  imagem:
+    "https://sportshub.cbsistatic.com/i/2021/09/18/5ff2dc8d-52b7-4e47-acce-080aa109444e/boruto-naruto-isshiki-otsutsuki-anime-spoilers.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 23,
+    poder: 35
+  }
+};
+var carta13 = {
+  nome: "Boruto Karma",
+  imagem:
+    "https://ovicio.com.br/wp-content/uploads/2020/03/20200320-boruto-chapter-44-spoilers.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 30,
+    poder: 28
+  }
+};
+var carta14 = {
+  nome: "kawaki karma",
+  imagem:
+    "https://images.everyeye.it/img-notizie/boruto-naruto-next-generations-capitolo-31-emersi-sentimenti-naruto-v3-362638.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 25,
+    poder: 19
+  }
+};
+var carta15 = {
+  nome: "mitsuki",
+  imagem:
+    "https://static2.cbrimages.com/wordpress/wp-content/uploads/2019/04/Mitsuki-Sage.jpg",
+  atributos: {
+    ataque: 45,
+    defesa: 10,
+    poder: 20
+  }
+};
+var carta16 = {
+  nome: "Asta",
+  imagem:
+    "https://i.pinimg.com/originals/cc/55/ec/cc55ecb794c1a222a2727229207063b9.jpg",
+  atributos: {
+    ataque: 52,
+    defesa: 60,
+    poder: 10
+  }
+};
+var carta17 = {
+  nome: "Yami",
+  imagem:
+    "https://cdn.videotapenews.com/wp-content/uploads/2020/08/Yami-Sukehiro.jpg",
+  atributos: {
+    ataque: 49,
+    defesa: 15,
+    poder: 48
+  }
+};
+var carta18 = {
+  nome: "Noelle",
+  imagem:
+    "https://i.pinimg.com/originals/f3/7f/1e/f37f1e5f1e5fc8ae44c2a925020c570d.jpg",
+  atributos: {
+    ataque: 35,
+    defesa: 30,
+    poder: 28
+  }
+};
+var carta19 = {
+  nome: "William Vangeance",
+  imagem: "https://pbs.twimg.com/media/EeLWEGiUYAArkq6.jpg",
+  atributos: {
+    ataque: 48,
+    defesa: 20,
+    poder: 48
+  }
+};
+var carta20 = {
+  nome: "Yuno",
+  imagem:
+    "https://www.comboinfinito.com.br/principal/wp-content/uploads/2019/06/black-clover-header.jpg",
+  atributos: {
+    ataque: 47,
+    defesa: 24,
+    poder: 29
+  }
+};
+var carta21 = {
+  nome: "Sukuna",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/09/Sukuna_popularidade.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 30,
+    poder: 40
+  }
+};
+var carta22 = {
+  nome: "Yuji",
+  imagem: "https://criticalhits.com.br/wp-content/uploads/2021/06/Yuji.jpg",
+  atributos: {
+    ataque: 31,
+    defesa: 43,
+    poder: 22
+  }
+};
+var carta23 = {
+  nome: "Megumi",
+  imagem: "https://nerdhits.com.br/wp-content/uploads/2021/05/megumi-1.jpg",
+  atributos: {
+    ataque: 38,
+    defesa: 28,
+    poder: 30
+  }
+};
+var carta24 = {
+  nome: "Gojo",
+  imagem: "https://pbs.twimg.com/media/ExEo-9rWgAg_0J7.jpg",
+  atributos: {
+    ataque: 70,
+    defesa: 50,
+    poder: 50
+  }
+};
+var carta25 = {
+  nome: "Mahito",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/09/3095cc3eb4c4950d626d9c4c40674d50-910x512.jpg",
+  atributos: {
+    ataque: 30,
+    defesa: 46,
+    poder: 38
+  }
+};
+var carta26 = {
+  nome: "Dio",
+  imagem:
+    "http://image.uc.cn/s/wemedia/s/upload/2019/f3ef0ff1e43272f0e71c54e8b66f2827.jpg",
+  atributos: {
+    ataque: 39,
+    defesa: 35,
+    poder: 59
+  }
+};
+var carta27 = {
+  nome: "Jotaro",
+  imagem:
+    "https://pm1.narvii.com/7920/0dfbfc5acf5800467719e600a0ab782ae2484048r1-720-549v2_00.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 28,
+    poder: 59
+  }
+};
+var carta28 = {
+  nome: "Giorno",
+  imagem: "https://pbs.twimg.com/media/EAkNBbrXoAArTZm.jpg",
+  atributos: {
+    ataque: 39,
+    defesa: 19,
+    poder: 60
+  }
+};
+var carta29 = {
+  nome: "Bruno",
+  imagem:
+    "https://i.pinimg.com/originals/9b/e5/29/9be529836549a0dbe4e38b643b7d5773.jpg",
+  atributos: {
+    ataque: 29,
+    defesa: 25,
+    poder: 48
+  }
+};
+var carta30 = {
+  nome: "Diavolo",
+  imagem: "https://criticalhits.com.br/wp-content/uploads/2019/11/Diavolo.jpg",
+  atributos: {
+    ataque: 25,
+    defesa: 37,
+    poder: 53
+  }
+};
+var carta31 = {
+  nome: "Tanjiro",
+  imagem: "https://sm.ign.com/ign_br/screenshot/default/tanjiro_ef6a.jpg",
+  atributos: {
+    ataque: 31,
+    defesa: 40,
+    poder: 32
+  }
+};
+var carta32 = {
+  nome: "Muzan",
+  imagem:
+    "https://nerdhits.com.br/wp-content/uploads/2021/04/muzan-2-1200x675.jpg",
+  atributos: {
+    ataque: 42,
+    defesa: 31,
+    poder: 57
+  }
+};
+var carta33 = {
+  nome: "Inosuke",
+  imagem: "https://static.anime21.blog.br/2019/06/08-9.jpg",
+  atributos: {
+    ataque: 29,
+    defesa: 29,
+    poder: 30
+  }
+};
+var carta34 = {
+  nome: "Zenitsu",
+  imagem: "https://cosmonerd.com.br//uploads/2020/10/zenitsu-demonslayer.jpg",
+  atributos: {
+    ataque: 35,
+    defesa: 10,
+    poder: 32
+  }
+};
+var carta35 = {
+  nome: "Nezuko",
+  imagem: "https://sm.ign.com/ign_br/screenshot/default/nezuko1_8unu.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 30,
+    poder: 50
+  }
+};
+var carta36 = {
+  nome: "Saitama",
+  imagem:
+    "https://ptanime.com/wp-content/uploads/2015/10/One-Punch-Man-Aborrecido.jpg",
+  atributos: {
+    ataque: 70,
+    defesa: 50,
+    poder: 0
+  }
+};
+var carta37 = {
+  nome: "Genos",
+  imagem: "https://i.ytimg.com/vi/jjyTzL6sxdQ/maxresdefault.jpg",
+  atributos: {
+    ataque: 45,
+    defesa: 32,
+    poder: 47
+  }
+};
+var carta38 = {
+  nome: "Sonic velocidade da luz",
+  imagem:
+    "https://1.bp.blogspot.com/-qQTkahBRal0/XLdT402GqiI/AAAAAAAABqY/mKbHsIaHKGMkozRx40DS7vTzg8wVwLy4ACKgBGAs/w0/speed-o-sound-sonic-one-punch-man-uhdpaper.com-4K-1.jpg",
+  atributos: {
+    ataque: 23,
+    defesa: 19,
+    poder: 10
+  }
+};
+var carta39 = {
+  nome: "Tatsumaki",
+  imagem: "https://i.ytimg.com/vi/NseD4Vx3n-g/maxresdefault.jpg",
+  atributos: {
+    ataque: 37,
+    defesa: 29,
+    poder: 35
+  }
+};
+var carta40 = {
+  nome: "Garou",
+  imagem: "https://criticalhits.com.br/wp-content/uploads/2020/03/garou.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 23,
+    poder: 40
+  }
+};
+var carta41 = {
+  nome: "Luffy",
+  imagem: "https://criticalhits.com.br/wp-content/uploads/2020/04/luffy.jpg",
+  atributos: {
+    ataque: 52,
+    defesa: 42,
+    poder: 59
+  }
+};
+var carta42 = {
+  nome: "Zoro",
+  imagem:
+    "https://ovicio.com.br/wp-content/uploads/2021/07/20210712-one-piece-zoro-wano-postcover.jpg",
+  atributos: {
+    ataque: 49,
+    defesa: 51,
+    poder: 49
+  }
+};
+var carta43 = {
+  nome: "Ace",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2021/02/portgas-d-ace-hat-raining-one-piece-anime-18200.jpg",
+  atributos: {
+    ataque: 45,
+    defesa: 32,
+    poder: 49
+  }
+};
+var carta44 = {
+  nome: "Sanji",
+  imagem:
+    "https://geekdama.com.br/wp-content/uploads/2021/02/one-piece-sanji-postcover.jpg",
+  atributos: {
+    ataque: 48,
+    defesa: 52,
+    poder: 30
+  }
+};
+var carta45 = {
+  nome: "Barba Negra",
+  imagem:
+    "http://pm1.narvii.com/6262/8a34b70aa408c8c4033cb7f42d2d4fd5c1b625da_00.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 59,
+    poder: 41
+  }
+};
+var carta46 = {
+  nome: "Dark Deku",
+  imagem:
+    "https://designe.com.br/wp-content/uploads/2021/06/dark-deku-izuku-midoriya-boku-no-hero-academia-bnha-mha-my-hero-academia-317-780x420.jpg",
+  atributos: {
+    ataque: 56,
+    defesa: 49,
+    poder: 53
+  }
+};
+var carta47 = {
+  nome: "Deku",
+  imagem:
+    "https://sm.ign.com/ign_br/screenshot/default/my-hero-academia-one-for-all-100-e1578917325129_jhmu.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 45,
+    poder: 50
+  }
+};
+var carta48 = {
+  nome: "Todoroki",
+  imagem:
+    "https://images.everyeye.it/img-notizie/my-hero-academia-scovata-curiosa-somiglianza-shoto-fratello-v3-500317.jpg",
+  atributos: {
+    ataque: 45,
+    defesa: 30,
+    poder: 50
+  }
+};
+var carta49 = {
+  nome: "Shigaraki",
+  imagem:
+    "https://criticalhits.com.br/wp-content/uploads/2020/05/shigaraki-2.jpg",
+  atributos: {
+    ataque: 40,
+    defesa: 10,
+    poder: 34
+  }
+};
+var carta50 = {
+  nome: "Bakugou",
+  imagem:
+    "https://cutewallpaper.org/22/deku-and-bakugo-one-for-all-wallpapers/124615612.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 35,
+    poder: 48
+  }
+};
+var carta51 = {
+  nome: "Jin Mori",
+  imagem: "https://anidlencodes.xyz/wp-content/uploads/2020/09/GoH-13.jpg",
+  atributos: {
+    ataque: 39,
+    defesa: 49,
+    poder: 29
+  }
+};
+var carta52 = {
+  nome: "Yoo Mira",
+  imagem: "https://wallpapercave.com/wp/wp7836684.jpg",
+  atributos: {
+    ataque: 35,
+    defesa: 39,
+    poder: 36
+  }
+};
+var carta53 = {
+  nome: "Daewi",
+  imagem:
+    "https://i.pinimg.com/originals/f3/34/e4/f334e47f0598bacfbf4433d6857a2646.jpg",
+  atributos: {
+    ataque: 39,
+    defesa: 27,
+    poder: 40
+  }
+};
+var carta54 = {
+  nome: "Taejin",
+  imagem:
+    "https://64.media.tumblr.com/b5313147bc064cc842796f31b2fa0019/3097dc34764c3f3c-9a/s540x810/f2933c734a0cb8d9fc44eb80c621a0fcae0fd464.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 47,
+    poder: 32
+  }
+};
+var carta55 = {
+  nome: "Mandeok",
+  imagem:
+    "https://i0.wp.com/anitrendz.net/news/wp-content/uploads/2020/09/The-God-of-High-School-Episode-11-5.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 47,
+    poder: 32
+  }
+};
+var carta56 = {
+  nome: "Gildarts",
+  imagem:
+    "http://2.bp.blogspot.com/-lTmcglHGqJ8/UvKdG12CjwI/AAAAAAAAAcg/uFoqDN4dsmo/w1200-h630-p-k-no-nu/Gildarts-Aura-gildarts-clive-28458216-900-1032.jpg",
+  atributos: {
+    ataque: 46,
+    defesa: 52,
+    poder: 41
+  }
+};
+var carta57 = {
+  nome: "Dimaria",
+  imagem: "https://pbs.twimg.com/media/Eed1g-yUYAItVri.jpg",
+  atributos: {
+    ataque: 42,
+    defesa: 37,
+    poder: 37
+  }
+};
+var carta58 = {
+  nome: "Natsu",
+  imagem:
+    "https://www.epicdope.com/wp-content/uploads/2020/07/Etherious-Natsu-Dragneel.jpg",
+  atributos: {
+    ataque: 50,
+    defesa: 40,
+    poder: 29
+  }
+};
+var carta59 = {
+  nome: "Igneel",
+  imagem:
+    "http://pm1.narvii.com/6653/c52b4005c3037a799f7ede45272586b73b7a8b57_00.jpg",
+  atributos: {
+    ataque: 51,
+    defesa: 17,
+    poder: 38
+  }
+};
+var carta60 = {
+  nome: "Larcade",
+  imagem:
+    "https://i.pinimg.com/originals/e8/12/72/e812724b962d357f82a314aa96605788.jpg",
+  atributos: {
+    ataque: 49,
+    defesa: 20,
+    poder: 47
+  }
+};
+var carta61 = {
+  nome: "Mob",
+  imagem:
+    "https://ultimatodobacon.com/wp-content/uploads/2020/07/MOB-PSYCHO-100-MANGA-ONE-1024x653.jpg",
+  atributos: {
+    ataque: 60,
+    defesa: 50,
+    poder: 56
+  }
+};
+var carta62 = {
+  nome: "Ekubo",
+  imagem:
+    "https://lostinanime.com/wp-content/uploads/2016/09/Mob-Psycho-100-10-9.jpg",
+  atributos: {
+    ataque: 10,
+    defesa: 32,
+    poder: 21
+  }
+};
+var carta63 = {
+  nome: "Sho Suzuki",
+  imagem:
+    "https://i.pinimg.com/originals/e5/ba/dd/e5badd1ee0f7ce55367788eb524abd71.jpg",
+  atributos: {
+    ataque: 45,
+    defesa: 28,
+    poder: 47
+  }
+};
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20,
+  carta21,
+  carta22,
+  carta23,
+  carta24,
+  carta25,
+  carta26,
+  carta27,
+  carta28,
+  carta29,
+  carta30,
+  carta31,
+  carta32,
+  carta33,
+  carta34,
+  carta35,
+  carta36,
+  carta37,
+  carta38,
+  carta39,
+  carta40,
+  carta41,
+  carta42,
+  carta43,
+  carta44,
+  carta45,
+  carta46,
+  carta47,
+  carta48,
+  carta49,
+  carta50,
+  carta51,
+  carta52,
+  carta53,
+  carta54,
+  carta55,
+  carta56,
+  carta57,
+  carta58,
+  carta59,
+  carta60,
+  carta61,
+  carta62,
+  carta63
+];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * 63);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * 63);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    numeroCartaJogador = parseInt(Math.random() * 63);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtrbtSelcnd() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtrbtSelcnd();
+  var elementoResultado = document.getElementById("resultado");
+  var valorCartaJgdr = cartaJogador.atributos[atributoSelecionado];
+  var valorCartaMaqn = cartaMaquina.atributos[atributoSelecionado];
+  if (valorCartaJgdr > valorCartaMaqn) {
+    elementoResultado.innerHTML = "Você Venceu! :) ";
+  } else if (valorCartaMaqn > valorCartaJgdr) {
+    elementoResultado.innerHTML = "Você Perdeu! :( ";
+  } else {
+    elementoResultado.innerHTML = "Empatou :/ ";
+  }
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "' >" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p> `;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+  var moldura =
+    '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "' >" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p> `;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
